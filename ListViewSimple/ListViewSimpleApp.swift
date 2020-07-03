d8be544a69b07225b23f8699ef6ad66ee3550aae
thanhsangvo <pl.thanhsang0710@gmail.com>
2020-07-03 23:08:33 +0700

    Initial Commit

---
+++ ListViewSimple/ListViewSimpleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListViewSimpleApp.swift
+//  ListViewSimple
+//
+//  Created by Võ Thanh Sang on 7/3/20.
+//
+
+import SwiftUI
+
+@main
+struct ListViewSimpleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
